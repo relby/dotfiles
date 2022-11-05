db7ec7bfad20c1776e01f9b3eb21ebe648f91091
relby <kudinov.nikita@gmail.com>
2022-11-06 02:01:21 +0300

    fix few issues

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ git clone https://aur.archlinux.org/paru.git /tmp/paru
 (cd /tmp/paru && makepkg -si)
 
 # Install packages
-sudo paru -S --needed - < ./pkglist.txt
+paru -S --needed - < ./pkglist.txt
 
 # Configure vscode 
-cat ./vscode/extensions.txt | grep ^[^#] | xargs -I {} code --install-extension {} --force
-cp -f ./vscode/settings.json "~/.config/Code - OSS/User"
+cat ./vscode/extensions.txt | grep "^[^#]" | xargs -I {} code --install-extension {} --force
+cp -f ./vscode/settings.json ~/.config/Code\ -\ OSS/User
 
 # Install .config folder
 cp -rf ./.config ~/
@@ -23,5 +23,6 @@ ssh-keygen -t rsa -b 4096 -C "kudinov.nikita@gmail.com"
 # Configure git
 git config --global user.name "relby"
 git config --global user.email "kudinov.nikita@gmail.com"
+git config --global init.defaultBranch "main"
 gh auth login
 
